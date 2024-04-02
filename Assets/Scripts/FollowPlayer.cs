db7b2ff86ceda03dc7dd13bad5d7247a09898932
awesomeadi00 <adityapandhare44@gmail.com>
2024-04-02 22:20:15 +0400

    Final touches on environment

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -17,7 +17,13 @@ public class FollowPlayer : MonoBehaviour
     {
         // This takes the player's speed into account of how far the camera will be for a more realistic effect
         Vector3 playerForward =(playerRb.velocity + player.transform.forward).normalized;
-        transform.position = Vector3.Lerp(transform.position, player.transform.position + player.transform.TransformVector(offset) + playerForward * (-5f), speed * Time.deltaTime);
+        Vector3 targetPosition = player.transform.position + player.transform.TransformVector(offset) + playerForward * (-5f);
+        float distance = Vector3.Distance(transform.position, targetPosition);
+
+        // Calculate a dynamic speed based on the distance.
+        float dynamicSpeed = Mathf.Clamp(distance / 10.0f, 0.1f, speed); // Adjust these values as needed.
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, dynamicSpeed * Time.deltaTime);
         transform.LookAt(player.transform);
     }
 }
